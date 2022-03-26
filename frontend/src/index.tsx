91df99d151d7a5d0838a50c287d1e07f02b5b144
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2022-03-27 03:49:39 +0700

    Update index.tsx
    - Remove <App />
    - Change into <MainRouter />

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //Router
 import { BrowserRouter } from 'react-router-dom';
+import MainRouter from 'router/router';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from 'redux/store';
 
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <MainRouter />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
