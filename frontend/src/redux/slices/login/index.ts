43380d3e1303d3146f19f4c1055171ef4e69af4d
antruong <34498550+AnTruongQuoc@users.noreply.github.com>
2022-03-28 17:09:33 +0700

    Update index.ts
    - add demoLogin functions in reducer

--- frontend/src/redux/slices/login/index.ts
+++ frontend/src/redux/slices/login/index.ts
@@ -1,6 +1,17 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { UserLogin } from "../../types/LoginType";
 
+const mockUser = [
+    {
+        email: 'truongquocan123@gmail.com',
+        password: '123456',
+    },
+    {
+        email: 'demo@yoo.com',
+        password: '123456',
+    }
+]
+
 const initialState: UserLogin = {
     email: "",
     isLogin: false,
@@ -18,10 +29,17 @@ const userLoginSlice = createSlice({
             state.isLogin = false;
             state.isLoading = false;
             state.error = "";
+        },
+        demoLogin: (state: UserLogin, action: PayloadAction<{email: string, password: string}>) => {
+            mockUser.forEach(user => {
+                if (user.email === action.payload.email && user.password === action.payload.password) {
+                    state.isLogin = true;
+                }
+            })
         }
     },
     extraReducers: {}
 });
 
-export const { logout } = userLoginSlice.actions;
+export const { logout, demoLogin } = userLoginSlice.actions;
 export default userLoginSlice.reducer;
