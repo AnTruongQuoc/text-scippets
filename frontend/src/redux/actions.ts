85c61eafc400630ea7335a8d152c168973a563c1
antruong <34498550+AnTruongQuoc@users.noreply.github.com>
2022-03-30 17:55:22 +0700

    Update actions.ts
    - create userLoginThunkCreator

--- frontend/src/redux/actions.ts
+++ frontend/src/redux/actions.ts
@@ -1,3 +1,13 @@
 // Export all needed actions here
 
+/**
+ * Actions of user
+ */
 export {logout, demoLogin} from './slices/login/index';
+
+export const userLoginThunkCreator = (params: {email:string, password: string}) => {
+    return {
+        type: 'login',
+        payload: params
+    }
+}
